52207bcf77134c4f2fa82a294be0f4551178e2c7
John Boiles <johnaboiles@gmail.com>
2022-01-26 16:15:44 -0800

    Use my fork of s-moch/CEC

--- components/hdmi_cec/__init__.py
+++ components/hdmi_cec/__init__.py
@@ -105,9 +105,9 @@ async def to_code(config):
     await cg.register_component(var, config)
 
     cg.add_library(
-        "",
-        "",
-        "https://github.com/s-moch/CEC.git#bfbc1ac4ea3419af3943851de582a80dde0d67aa",
+        None,
+        None,
+        "https://github.com/johnboiles/CEC.git#d4b642c90f48f4e46c2cb8c234ab6dd2e5aee7c5",
     )
     cg.add(var.set_address([config[CONF_ADDRESS]]))
     cg.add(var.set_physical_address([config[CONF_PHYSICAL_ADDRESS]]))
